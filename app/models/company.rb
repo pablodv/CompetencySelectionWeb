b676354244f1266a8cef8e430efa493f95107537
Pablo <pablo.moreiramora@gmail.com>
2010-12-25 12:38:06 -0300

    Add flash message

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,7 @@ class Company < ActiveRecord::Base
   has_many :vacancies
 
   validates_uniqueness_of :name, :login
+  validates_presence_of  :name
   
   has_friendly_id :name, :use_slug => true
 end
