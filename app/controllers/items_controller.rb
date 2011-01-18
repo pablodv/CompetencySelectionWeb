669d96f23374af21a0ec5e71ed9d7f0700ee9abf
Pablo <pablo.moreiramora@gmail.com>
2011-01-17 22:21:58 -0300

    Integrated core controller with jobs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,4 @@
 class ItemsController < CoreController
   belongs_to :competency
 
-#  protected
-#
-#  def begin_of_association_chain
-#    @current_competency = Competency.find(params[:competency_id])
-#  end
-
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,21 +1,4 @@
 class JobsController < CoreController
-
-#  def create
-#    create! do |format|
-#      format.html { redirect_to user_jobs_path }
-#    end
-#  end
-#
-#  def update
-#    update! do |format|
-#      format.html { redirect_to user_jobs_path }
-#    end
-#  end
-
- protected
-
-  def begin_of_association_chain
-    current_user
-  end
+  belongs_to :user
 
 end
